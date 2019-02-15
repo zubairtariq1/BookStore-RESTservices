34c01b0cb878c18e3915087e3a89ddb80b5d516f
Zubair Tariq <zubair_success747@yahoo.com>
2019-02-15 13:13:13 +0200

    Added REST services to fetch books by ID and all the books as well

--- src/main/java/study/app/bookstore/web/BookController.java
+++ src/main/java/study/app/bookstore/web/BookController.java
@@ -1,11 +1,16 @@
 package study.app.bookstore.web;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 
 import study.app.bookstore.domain.Book;
 import study.app.bookstore.domain.BookRepository;
@@ -24,6 +29,18 @@ public class BookController {
 	model.addAttribute("books", repository.findAll());
 	return "bookList";
 	}
+	
+	// RESTful service to get all Books
+    @RequestMapping(value="/books", method = RequestMethod.GET)
+    public @ResponseBody List<Book> bookStoreRest() {	
+        return (List<Book>) repository.findAll();
+    }    
+
+	// RESTful service to get Book by id
+    @RequestMapping(value="/book/{id}", method = RequestMethod.GET)
+    public @ResponseBody Optional<Book> findBookRest(@PathVariable("id") Long bookId) {	
+    	return repository.findById(bookId);
+    }
 	
 	@RequestMapping(value = "/addBook")
 	public String addBook(Model model){
